4002cf0c99a96fb66f50308c9155415549a4a8e1
Marshyy <marshmelloweeee@gmail.com>
2023-04-18 08:17:38 -0300

    estáaparecendo para preencher os campos vazios

--- cadastro.js
+++ cadastro.js
@@ -2,9 +2,11 @@ function Verificar(){
 
     let email = document.getElementById('CadastroEmail').value;
     let senha = document.getElementById('CadastroSenha').value;
+    let Rua = document.getElementById('Adress').value;
+    let Telephone = document.getElementById('number').value;
     console.log(email);
     
-    if(!email || !senha){
+    if(!email || !senha || !Rua || !Telephone){
         alert("campos de preenchimento obrigatório. porfavor preencher");
     }
 
