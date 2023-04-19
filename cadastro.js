8d7a06dbf7629810a724bcad53683cdab2e76e3b
Marshyy <marshmelloweeee@gmail.com>
2023-04-19 09:51:34 -0300

    Agora com CEP incluido e javascript escrito

--- cadastro.js
+++ cadastro.js
@@ -3,9 +3,11 @@ function Verificar(){
     let email = document.getElementById('CadastroEmail').value;
     let senha = document.getElementById('CadastroSenha').value;
     let telephone = document.getElementById('number').value;
+    let CPF_CNPJ = document.getElementById('CPF_CNPJ').value;
+    let CEP = document.getElementById('CEP').value;
     console.log(email);
     
-    if(!email || !senha || !telephone){
+    if(!email || !senha || !telephone || !CPF_CNPJ || !CEP){
         alert("campos de preenchimento obrigatório. porfavor preencher");
     }
 
