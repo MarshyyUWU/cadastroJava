0da1b4be131d93c5ee19502f5dc10b73873676c4
Marshyy <marshmelloweeee@gmail.com>
2023-04-26 10:57:17 -0300

    agora com a confirmação de senha sem utilização

--- cadastro.js
+++ cadastro.js
@@ -1,33 +1,33 @@
 function Verificar(){
 
     let email = document.getElementById('CadastroEmail').value;
-    let senha = document.getElementById('CadastroSenha').value;
+    let senha = document.getElementById('password').value;
+    let senha2 = document.getElementById('confirm_password').value;
     let telephone = document.getElementById('number').value;
     let CPF_CNPJ = document.getElementById('CPF_CNPJ').value;
-    let CEP = document.getElementById('CEP').value;
-    console.log(email);
+    // let CEP = document.getElementById('CEP').value;
    
-    // declarando as variaveis ^^ 
+    // declarando as variaveis ^^ || !CEP
 
     if(!email || !senha || !telephone || !CPF_CNPJ || !CEP){
         alert("campos de preenchimento obrigatório. porfavor preencher");
-    }
+    }else{
+      alert("Campos preenchidos com sucesso");
 
+      if(senha.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Senhas diferentes!");
+      } else {
+        confirm_password.setCustomValidity('');
+      }
+    }  
+
+  }
 
     // o código abaixo foi pego de um site, serve para confirmar a senha.
 
-    var password = document.getElementById("password")
-  , confirm_password = document.getElementById("confirm_password");
+      var password = document.getElementById("password")
+    , confirm_password = document.getElementById("confirm_password");
 
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Senhas diferentes!");
-  } else {
-    confirm_password.setCustomValidity('');
-  }
-}
-
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
-
-}
+  
+  password.onchange = validatePassword;
+  confirm_password.onkeyup = validatePassword;
